80377510a4655629c4a8f2d55afe57f363b112bf
Thomas Willson <thomas.willson@me.com>
2015-11-30 21:47:27 -0800

    Fix efficiency calculation for electric car.

--- Class Definitions/Telemetry.m
+++ Class Definitions/Telemetry.m
@@ -10,9 +10,9 @@ classdef Telemetry < handle
         Miscellaneous = {}; %Msc data stored here, e.g. brake points etc
         Results = {}; % Processed simulator results, e.g. top speed, lap time etc
         LapData = []; % Lap data stored in a matrix with columns in the following order
-                      % [Position,Velocity,Long. Gs, Lat. Gs, Traction
-                      % Limit Reached, Wheel RPM, Motor RPM, Motor Power, Motor
-                      % Torque, Braking Torque, Time, Efficiency]
+        % [Position,Velocity,Long. Gs, Lat. Gs, Traction
+        % Limit Reached, Wheel RPM, Motor RPM, Motor Power, Motor
+        % Torque, Braking Torque, Time, Efficiency]
         LapDataStructure = [];
     end
     
@@ -33,26 +33,26 @@ classdef Telemetry < handle
             % the given entrance and exit velocities.
             %
             % INPUTS
-            % Name          Type          Units   Description            
+            % Name          Type          Units   Description
             %**************************************************************
             % Tele         Telemetry      N/A     Telemetry object with
-            %                                     brake point, entry v, 
+            %                                     brake point, entry v,
             %                                     exit v data
             % Track         Test Track    N/A     Test track object that
-            %                                     has individual section 
+            %                                     has individual section
             %                                     data from the simulator
             %
             % OUTPUTS
-            % Name          Type          Units   Description            
+            % Name          Type          Units   Description
             %**************************************************************
             % NONE
             %
             % VARIABLES
-            % Name          Type          Units   Description            
+            % Name          Type          Units   Description
             %**************************************************************
             % EntranceV     [Nx1] array   in/s    Entrance velocities for
             %                                     each track section
-            % ExitV         [Nx1] array   in/s    Exit velocities for each 
+            % ExitV         [Nx1] array   in/s    Exit velocities for each
             %                                     track section
             % BP            [Nx1] array   in      Braking points for each
             %                                     track section
@@ -61,7 +61,7 @@ classdef Telemetry < handle
             %                                     and total track length
             % LapDataStitch [Mx11]array   N/A     Lap Data Matrix
             % Acc           [Lx2] array   N/A     Throttle curve giving
-            %                                     in/s vs in for each track 
+            %                                     in/s vs in for each track
             %                                     section
             % Dec           [Lx2] array   N/A     Braking curve for each
             %                                     track section
@@ -69,30 +69,30 @@ classdef Telemetry < handle
             % L             float         in      Running total of track
             %                                     length
             % I             int           N/A     Indexing variable for
-            %                                     positioning throttle and 
+            %                                     positioning throttle and
             %                                     brake curves on track
             %                                     section
-            % dl             float        in      Distance throttle and 
+            % dl             float        in      Distance throttle and
             %                                     brake curves
-            %                                     needs to be shifted to 
-            %                                     match with entrance and 
+            %                                     needs to be shifted to
+            %                                     match with entrance and
             %                                     exit velocities
             % X             [Kx1] array   in      Array of position
-            %                                     increments to append to 
-            %                                     throttle/brake curves if 
+            %                                     increments to append to
+            %                                     throttle/brake curves if
             %                                     necessary
             %
             % FUNCTIONS
-            % Name          Location         Description            
+            % Name          Location         Description
             %**************************************************************
-            % NONE   
+            % NONE
             
-            % Pulls entrance and exit velocities, and brake points 
-            % for each section from telemetry object.  
+            % Pulls entrance and exit velocities, and brake points
+            % for each section from telemetry object.
             EntranceV = cell2mat(Tele.Miscellaneous(1));
             ExitV = cell2mat(Tele.Miscellaneous(2));
             BP = cell2mat(Tele.Miscellaneous(3));
-           
+            
             % Find the number of track sections to properly size arrays and
             % for loop iterations
             S = Track.Sections;
@@ -103,39 +103,39 @@ classdef Telemetry < handle
             RL(1,1) = Track.Track(1).Length; % First section length
             RL(1,2) = RL(1,1); % First running total
             for i = 2:S
-
+                
                 RL(i,2) = Track.Track(i).Length; % Current section length
-                RL(i,1) = RL(i-1,1) + RL(i,2); % Running total is equal to 
-                                               % the previous running total 
-                                               % plus the current section 
-                                               % length
-
+                RL(i,1) = RL(i-1,1) + RL(i,2); % Running total is equal to
+                % the previous running total
+                % plus the current section
+                % length
+                
             end
-
-
+            
+            
             LapDataStitch = []; % Initialize lap data matrix variable
-
+            
             for i = 1:S % For each one of the track sections
-
+                
                 Acc = (Track.Track(i).AccCurve); % Pull throttle curve
                 Dec = (Track.Track(i).DecCurve); % Pull brake curve
                 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
-%dx affected by shifting 
-
-                dx = Acc(2,1) - Acc(1,1); % Determine the position 
-                                          % increment that the simulator 
-                                          % used
-
+                %dx affected by shifting
+                
+                dx = Acc(2,1) - Acc(1,1); % Determine the position
+                % increment that the simulator
+                % used
+                
                 L = RL(i,2); % Pull the current section length
                 
-                % Find the point in the section throttle curve that matches 
+                % Find the point in the section throttle curve that matches
                 % the section's entrance velocity
                 I = find(Acc(:,2) <= EntranceV(i),1,'last');
                 % Find the length that the curve needs to be shifted to
                 % match
                 dl = Acc(I,1);
                 Acc(:,1) = Acc(:,1) - dl; % Shift throttle curve positions
-
+                
                 % If the ending throttle curve position is less than the
                 % length of the section
                 if Acc(end,1) < round(L)
@@ -153,13 +153,13 @@ classdef Telemetry < handle
                     MT = ones(length(X),1).*Acc(end,9);
                     BT = zeros(length(X),1);
                     NewT  = ones(length(X),1).*Acc(end,11);
-                   
+                    
                     % Create data appendix
                     Appendix = [X,V,FG,LG,TL,WR,MR,P,MT,BT,NewT];
                     % Append data
                     Acc = [Acc;Appendix];
                 end
-
+                
                 % If the end of the throttle curve is longer than the
                 % length of the track, trim it to fit.
                 if Acc(end,1) > L
@@ -167,7 +167,7 @@ classdef Telemetry < handle
                     I = I + 1;
                     Acc(I:end,:) = [];
                 end
-
+                
                 % Find the point in the section brake curve that matches
                 % with the section exit velocity
                 I = find(Dec(:,2) >= ExitV(i),1,'last');
@@ -175,19 +175,19 @@ classdef Telemetry < handle
                     % find the length that the curve needs to be shifted
                     dl = Dec(I,1) - L;
                     dl = dx*round(dl/dx);
-                else % if no index is found, then this implies that the 
+                else % if no index is found, then this implies that the
                     % exit velocity is higher than the start of the brake
                     % curve likely due to rounding errors.  If this
                     % happens, then shift the brake curve to start at the
                     % end of the section
                     dl = L;
                 end
-
+                
                 % shift curve
                 Dec(:,1) = Dec(:,1) - dl;
-
+                
                 % If the brake curve has been shifted past the start of the
-                % section, then append data to start.  
+                % section, then append data to start.
                 if Dec(1,1) > 0
                     % Carry through the starting values of the curve up to
                     % the beginning
@@ -205,7 +205,7 @@ classdef Telemetry < handle
                     Appendix = [X,V,FG,LG,TL,WR,MR,P,MT,BT,NewT];
                     Dec = [Appendix;Dec];
                 end
-
+                
                 % If the end of the brake curve is past the end of the
                 % section, trim the end to match the total section length
                 if Dec(end,1) > L
@@ -213,7 +213,7 @@ classdef Telemetry < handle
                     I = I + 1;
                     Dec(I:end,:) = [];
                 end
-
+                
                 % Find the start and end indexes of the throttle curve
                 % between the start of the section and up to the brake
                 % point
@@ -229,19 +229,19 @@ classdef Telemetry < handle
                 % ending at the end of the section
                 DecStartIndex = find(Dec(:,1) == Acc(AccEndIndex,1)+dx,1,'first');
                 DecEndIndex = find(Dec(:,1) <= L,1,'last');
-
+                
                 % Append throttle and brake curve sections together for the
                 % section data
                 SectionData = [Acc(AccStartIndex:AccEndIndex,:);...
                     Dec(DecStartIndex:DecEndIndex,:)];
-
+                
                 % For all sections except for the first, add the running
                 % total of the track length to the position column of the
-                % data matrix 
+                % data matrix
                 if i ~= 1
                     SectionData(:,1) = SectionData(:,1) + RL(i-1,1);
                 end
-
+                
                 % If the end of the section data is equal to the length of
                 % the running total of the track, then trim the last row of
                 % the data matrix.  This eliminates duplicate points in the
@@ -249,29 +249,29 @@ classdef Telemetry < handle
                 if SectionData(end,1) == RL(i,1)
                     SectionData(end,:) = [];
                 end
-
+                
                 % Appends section data to the end of the total track data
                 % matrix
                 LapDataStitch = [LapDataStitch;SectionData];
-
+                
             end
-
+            
             % Recalculates time increments to smooth out section borders
             % This is accomplished using dt = integrate(dx/V)
             
             % Create average velocity array for each position increment
             V1 = LapDataStitch(1:end-1,2);
             V2 = LapDataStitch(2:end,2);
-
+            
             Vavg = (V2 + V1)/2;
             
             % Create new time increment array by dividing position
             % increment by average velocity array
             NewT = dx./Vavg;
-
+            
             % Replace old time data with new time data
             LapDataStitch(2:end,11) = NewT;
-
+            
             % Recalculates Gs to smooth out borders between lap sections
             NewGs = (LapDataStitch(2:end,2) - LapDataStitch(1:end-1,2))./LapDataStitch(2:end,11);
             LapDataStitch(2:end,3) = NewGs/(12*32.174);
@@ -322,17 +322,17 @@ classdef Telemetry < handle
         function LapResultCalculator(Tele,Track,Gs,Car)
             
             S = Track.Sections;
-
+            
             Time = Tele.LapData(:,11);
-
+            
             TotalTime = sum(Time);
-
+            
             disp(['Total Lap Time (s)   : ', num2str(TotalTime)]);
             
             Energy = Tele.LapDataStructure.BatteryPower .* Tele.LapDataStructure.Time;
-
+            
             TotalEnergy = sum(Energy)/3600/1000;
-
+            
             disp(['Total Lap Energy (kWh): ', num2str(TotalEnergy)]);
             
             EnduranceLength = 866142; % 22km in inches
@@ -342,48 +342,48 @@ classdef Telemetry < handle
             Power = Tele.LapData(:,8);
             Power = Power*0.000112985;
             Energy = (Power.*Time);
-
+            
             I = Tele.LapData(:,5) == 1;
-
+            
             TimeTTL = sum(Tele.LapData(I,11));
-
+            
             PercentTL = TimeTTL/TotalTime * 100;
-
-%             disp(['% Traction Limit     : ', num2str(PercentTL)]);
-
+            
+            %             disp(['% Traction Limit     : ', num2str(PercentTL)]);
+            
             avgLongG = mean(abs(Tele.LapData(:,3)));
             avgLatG  = mean(abs(Tele.LapData(:,4)));
-
+            
             disp(['Average Long Gs      : ', num2str(avgLongG)])
             disp(['Average Lat Gs       : ', num2str(avgLatG)])
-
+            
             V = Tele.LapData(:,2)*(3600/(5280*12));
             X = Tele.LapData(:,1)/12;
-
-%             figure
-%             plot(X,V);
-%             xlabel('Distance (ft)')
-%             ylabel('Speed (mph)')
-%             title('Lap Speeds')
-% 
-%             figure
-%             hist(Tele.LapData(:,7),100);
-%             xlabel('Motor Speed (RPM)')
-%             ylabel('Number of Occurrences')
-%             title('Motor RPM Histogram')
-
+            
+            %             figure
+            %             plot(X,V);
+            %             xlabel('Distance (ft)')
+            %             ylabel('Speed (mph)')
+            %             title('Lap Speeds')
+            %
+            %             figure
+            %             hist(Tele.LapData(:,7),100);
+            %             xlabel('Motor Speed (RPM)')
+            %             ylabel('Number of Occurrences')
+            %             title('Motor RPM Histogram')
+            
             for i = 1:S
-
+                
                 R = Track.Track(i).Radius;
-
+                
                 if ~R
                     Acc = Track.Track(i).AccCurve;
-
+                    
                     TopV = (Acc(end,2))*(3600/(5280*12));
                     TimeV = sum(Acc(:,11));
                     DistV = (Acc(end,1))/(12*3.281);
                     TopA = max(Acc(:,3));
-
+                    
                     dl = 75 - DistV;
                     if dl < 0
                         D = Acc(:,1)/(12*3.281);
@@ -396,19 +396,19 @@ classdef Telemetry < handle
                         end
                     else
                         dt = dl/(TopV*0.44704);
-
+                        
                         TotalAccTime = TimeV + dt;
                     end
-
+                    
                     disp(['75m Run Time (s)     : ', num2str(TotalAccTime)])
-                    disp(['TTTS (s)             : ', num2str(TimeV)])        
+                    disp(['TTTS (s)             : ', num2str(TimeV)])
                     disp(['Top Speed (mph)      : ', num2str(TopV)])
                     disp(['DTTS (m)             : ', num2str(DistV)])
                     disp(['Max Acceleration (G) : ', num2str(TopA)])
-
+                    
                     break
                 end
-
+                
             end
             
             Tmin = 4.070;
@@ -423,7 +423,7 @@ classdef Telemetry < handle
             
             Tmin = 76.908;
             Tmax = 1.45*Tmin;
-
+            
             PerformancePoints = 142.5*(Tmax/TotalTime - 1)/(Tmax/Tmin - 1);
             if PerformancePoints < 0
                 PerformancePoints = 0;
@@ -447,33 +447,33 @@ classdef Telemetry < handle
             
             TminEnd = 73.48;
             Tmax = 1.45*TminEnd;
-                    if TotalTime > Tmax
-                        EndScore = EnduranceLaps;
-                    else
-                        EndScore = 250*(((Tmax/TotalTime) - 1)/((Tmax/TminEnd) - 1)) + 50;
-                    end
-                    
-                   disp(['Endurance Score       : ', num2str(EndScore)])
-                   
-                EFmin = 0.36;
-                EFmax = 0.93;
-                CO2min = .3594;
-                switch Car.TabName
-                    case 'Combustion'
-                        CO2kg = Car.Battery.Capacity*2.3/EnduranceLaps;
-                    case 'Electric'
-                        CO2kg = Car.Battery.Capacity*0.65/EndurancceLaps;
-                end
-                
-                EF = (TminEnd/TotalTime)*(CO2min/CO2kg);
-                EScore = 100*((EFmin/EF)-1)/((EFmin/EFmax)-1);
-                if EScore > 100
-                    EScore = 100;
-                elseif EScore < 0
-                    EScore = 0;
-                end
-                
-                disp(['Efficiency Score       : ', num2str(EScore)])
+            if TotalTime > Tmax
+                EndScore = EnduranceLaps;
+            else
+                EndScore = 250*(((Tmax/TotalTime) - 1)/((Tmax/TminEnd) - 1)) + 50;
+            end
+            
+            disp(['Endurance Score       : ', num2str(EndScore)])
+            
+            EFmin = 0.36;
+            EFmax = 0.93;
+            CO2min = .3594;
+            switch Car.TabName
+                case 'Combustion'
+                    CO2kg = Car.Battery.Capacity*2.3/EnduranceLaps;
+                case 'Electric'
+                    CO2kg = Car.Battery.Capacity*0.65/EnduranceLaps;
+            end
+            
+            EF = (TminEnd/TotalTime)*(CO2min/CO2kg);
+            EScore = 100*((EFmin/EF)-1)/((EFmin/EFmax)-1);
+            if EScore > 100
+                EScore = 100;
+            elseif EScore < 0
+                EScore = 0;
+            end
+            
+            disp(['Efficiency Score       : ', num2str(EScore)])
             
             TotalScore = AccScore + AutoXScore + SkidPadScore + EScore + EndScore;
             disp(['Total Score          : ', num2str(TotalScore)])
